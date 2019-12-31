81a169e1c6de424c9ab0c67409b2257084e7ce92
29chandu <scsalutagi@gmail.com>
2019-12-31 08:42:07 +0530

    Added update and delete functionality to posts

--- flask_blog/routes.py
+++ flask_blog/routes.py
@@ -119,11 +119,24 @@ def update_post(post_id):
 	if current_user != post.author:
 		abort(403)
 	form = PostForm()
-	form.title.data = post.title
-	form.content.data = post.content
 	if form.validate_on_submit():
 		post.title = form.title.data
 		post.content = form.content.data
 		db.session.commit()
+		flash('Your post has been updated!', 'success')
 		return redirect(url_for('post', post_id=post_id))
+	elif request.method == 'GET':
+		form.title.data = post.title
+		form.content.data = post.content
 	return render_template('create_post.html', legend='Update Post', title='Update Post', form=form)
+
+@app.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+	post = Post.query.get_or_404(post_id)
+	if current_user != post.author:
+		abort(403)
+	db.session.delete(post)
+	db.session.commit()
+	flash('Your post has been deleted!', 'success')
+	return redirect(url_for('home'))
